7322150cb157c64aa33cf28948e1692f82b40195
jorgegilramos <jorgegilramos@gmail.com>
2014-11-22 03:38:49 +0100

    Fix

--- js/min-resolution.js
+++ js/min-resolution.js
@@ -2,12 +2,12 @@
   window.Detector = (function(window, document, undefined){
     Detector = {};
     var maxRatio=4.0;
-    var supportDevicePixelRatio = function() {
+    Detector.supportDevicePixelRatio = function() {
       if (window.matchMedia) {
         var prefixes = "-webkit-min- min--moz- -o-min- -ms-min- -khtml-min- ".split(" ");
         var mediaQuery = "device-pixel-ratio:1.0";
         for (var i=0; i<prefixes.length; i++) {
-          if (window.matchMedia("only screen and (" + prefixes[i] + mediaQuery + ")").matches) {
+          if (window.matchMedia("(" + prefixes[i] + mediaQuery + ")").matches) {
             return prefixes[i];
           }
         }
@@ -16,12 +16,12 @@
     }();
     
     Detector.dpr = function() {
-      if (supportDevicePixelRatio) {
+      if (Detector.supportDevicePixelRatio) {
         var maxdpr = 1.0;
         var i=1.0;
         for (; i<=maxRatio; i=(i+0.1).toFixed(1)) {
-          console.log("trying dpr: " + i);
-          if (!window.matchMedia("(only screen and (" + supportDevicePixelRatio + "device-pixel-ratio:" + i + "))").matches) {
+          //console.log("trying dpr: " + i);
+          if (!window.matchMedia("(only screen and (" + Detector.supportDevicePixelRatio + "device-pixel-ratio:" + i + "))").matches) {
             break;
           } else {
             maxdpr = i;
